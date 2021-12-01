94398d243c2b832b9fc0e565817ce71da31c49f2
MattiDragon <matias.koivikko@gmail.com>
2021-12-01 18:42:42 +0200

    Update to 1.18

--- src/main/java/io/github/ph0t0shop/fabricacb/mixin/CommandManagerMixin.java
+++ src/main/java/io/github/ph0t0shop/fabricacb/mixin/CommandManagerMixin.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.CommandNode;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.github.ph0t0shop.fabricacb.FabricACB;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -20,14 +22,13 @@ public class CommandManagerMixin {
 		try {
 			 player = source.getPlayer();
 		} catch (CommandSyntaxException e) {
-			FabricACB.logger.warn("Trying to send command tree to non-player. Something is wrong!");
+			FabricACB.LOGGER.warn("Trying to send command tree to non-player. Something is wrong!");
 			return commandNode.canUse(source);
 		}
-
+		
 		if (commandNode instanceof LiteralCommandNode<ServerCommandSource> node) {
 			String literal = node.getLiteral();
-			if (FabricACB.commandBlacklist.contains(literal.toLowerCase()) &&
-					!FabricACB.serverInstance.getPlayerManager().isOperator(player.getGameProfile())) {
+			if (FabricACB.COMMAND_BLACKLIST.contains(literal.toLowerCase()) && !player.server.getPlayerManager().isOperator(player.getGameProfile())) {
 				return false;
 			}
 		}
